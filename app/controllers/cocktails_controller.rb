d4e25dd104cc862b50c7cb7bb6d4cd1f99f33257
Antal Bako <prowlersk8@gmail.com>
2020-11-20 11:06:35 +0100

    changed view

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -6,7 +6,34 @@ class CocktailsController < ApplicationController
 
   def show; end
 
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+    redirect_to cocktails_path
+  end
+
+
+  def edit; end
+
+  def update
+    @cocktail.update(cocktail_params)
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
   private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 
   def find_cocktail
     @cocktail = Cocktail.find(params[:id])
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -5,6 +5,31 @@ class DosesController < ApplicationController
     @doses = Dose.all
   end
 
+  def show; end
+
+  def new
+    @dose = dose.new
+  end
+
+  def create
+    @dose = dose.new(dose_params)
+    @dose.save
+    redirect_to doses_path
+  end
+
+
+  def edit; end
+
+  def update
+    @dose.update(dose_params)
+    redirect_to dose_path(@dose)
+  end
+
+  def destroy
+    @dose.destroy
+    redirect_to doses_path
+  end
+
   private
 
   def find_dose
